722eede1ca4c56586e88a6df83ecc7b77003c12e
kdasabhi <143336585+kdasabhi@users.noreply.github.com>
2025-12-28 04:17:55 +0530

    Create HappyNumber_Brute.java

---
+++ Day-3/HappyNumber_Brute.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int square(int n){
+        int sum=0;
+        while(n>0){
+            int digit=n%10;
+            sum+=digit*digit;
+            n=n/10;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+       Set<Integer> set=new HashSet<>();
+       while(n!=1){
+        if(set.contains(n)){
+            return false;
+        }
+        set.add(n);
+        n=square(n);
+       }
+       return true;
+    }
+}
